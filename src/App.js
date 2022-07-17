a73623792099eccdf63931f1d12e56fce921a40d
lasha tatulashvili <lashatatu@gmail.com>
2022-07-17 09:50:28 +0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router,Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar/>
-        <main>content</main>
+
+        <main className={'container mx-auto px-3 pb-12'}>Contenta</main>
+        <Footer/>
       </div>
     </Router>
   );
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -7,11 +7,9 @@ const Navbar = ({title}) => {
     <nav className={'navbar mb-12 shadow-lg bg-neutral text-neutral-content'}>
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
-
           <FaGithub className={'inline pr-2 text-3xl'}/>
           <Link to={'/'} className={'text-lg font-bold aling-midle'}>
             {title}
-
           </Link>
         </div>
         <div className="flex-1 px-2 mx-2">

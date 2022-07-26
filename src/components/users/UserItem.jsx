7f7d388532131f3f44de0283d58c85b3f2141992
lasha tatulashvili <lashatatu@gmail.com>
2022-07-26 15:42:59 +0400

    section 10 user profile top

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -20,7 +20,7 @@ const UserItem = ({
         <h2 className={'card-title'}>
           {login}
         </h2>
-        <Link className={'text-base-content text-opacity-40'} to={`/users/${login}`}>Visit Profile</Link>
+        <Link className={'text-base-content text-opacity-40'} to={`/user/${login}`}>Visit Profile</Link>
       </div>
 
     </div>
